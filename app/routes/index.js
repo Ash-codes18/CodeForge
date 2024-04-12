19bcd31af3c024c378ada9cabf6da8df5023ec44
Ranitpal-003-07 <ranitpal699@gmail.com>
2024-04-13 05:09:17 +0530

    added profile

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+//hackathon page
 router.get('/hackathon', function(req, res, next) {
   res.render('hackathon');
 });
